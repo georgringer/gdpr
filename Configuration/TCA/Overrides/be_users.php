c06dea49b73fbf82130e600c1279f4f50a6bcb49
Georg Ringer <georg.ringer@gmail.com>
2018-05-03 13:06:17 +0200

    [TASK] Use checkboxLabeledToggle

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -12,6 +12,21 @@ $fields = [
     ]
 ];
 
+if (version_compare(TYPO3_branch, '9.2', '>=')) {
+    $fields['gdpr_module_enable']['config'] = [
+        'type' => 'check',
+        'renderType' => 'checkboxLabeledToggle',
+        'items' => [
+            [
+                0 => '',
+                1 => '',
+                'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled'
+            ],
+        ],
+    ];
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', $fields);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'be_users',
